88d979a61e033149b06a6b5c133a3391f3bcbea1
Ciro-Lopez <cirobenjaminlopez2009@gmail.com>
2026-04-09 17:01:49 -0300

    Update receptor : Conteo de bits
    Se logra que el receptor capte la cantidad de bits que se emiten

--- receptor.ino
+++ receptor.ino
@@ -1,17 +1,35 @@
 const int ldrPin = A0;
 const int ledPin = 13;
+int lectura;
+bool iguales = true;
 
 int umbral = 500;
 bool espera = false;
-
 void setup() {
-    Serial.begin(9600);
-    pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  pinMode(ldrPin, INPUT);
+  Serial.println("--------------");
 }
 
 void loop() {
-    if (espera == false) {
-        espera = true;
+  lectura = analogRead(ldrPin);
+  Serial.println(lectura);
+  if (espera==false && lectura<20){
+    espera = true;
+  }
+  else
+  {
+    if (espera==true){
+      if (lectura>30 and lectura<100){
+        Serial.println("Se detecto 0");
+        espera = false;
+      }
+      else{if(lectura>120){
+        Serial.println("Se detecto 1");
+        espera = false;
+      }
+      }
     }
-    if (espera && analogRead(ldrPin))
+  }
+
 }
