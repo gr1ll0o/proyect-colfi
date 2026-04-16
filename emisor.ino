47c2eb2932880f648e37145e0803c1482413c628
yagosayas123 <yagosayas@gmail.com>
2026-04-16 17:51:22 -0300

    update emisor: first sucessfull try

--- emisor.ino
+++ emisor.ino
@@ -2,11 +2,22 @@
 int a;
 int led = 3;
 int brillo;
-String texto = "101000001";
+String texto = "101000011";
 int boton=8;
 int er=0;
-//entrada del enter
-int estadoBoton = digitalRead(boton);
+int estadoBoton = digitalRead(boton); //entrada del enter
+
+struct Diccionario {
+  String clave;
+  int valor;
+};
+
+Diccionario datos[] = {
+  {"a", "01000001"},
+  {"b", ""},
+  {"c", 3}
+};
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(led, OUTPUT);
@@ -15,7 +26,9 @@ void setup() {
 }
 
 void loop() {
-  analogWrite(led, 0);
+  Serial.println("");
+  Serial.println("ESPERANDO INCIO SECUENCIA");
+  digitalWrite(led, LOW);
   delay(1000);
   if (estadoBoton == 0) {
     while(er==0)
@@ -24,25 +37,27 @@ void loop() {
       if(estadoBoton==1)
       {
         er+=1;
+        digitalWrite(led, HIGH);
       }
     }
   }
+  digitalWrite(led, LOW);
   for (int i = 0; i < texto.length(); i++) {
-    analogWrite(led, 0);
+    digitalWrite(led, LOW);
     delay(500);
     char c = texto.charAt(i); // Obtiene el carácter en la posición i
     Serial.print("Caracter: ");
     Serial.println(c);
     if (c == '0') {
-      analogWrite(led, 0);
+      digitalWrite(led, LOW);
     }
     if (c== '1'){
-      analogWrite(led, 255);
+      digitalWrite(led, HIGH);
     }
     delay(500);
   }
-  analogWrite(led, 0);
-  estadoBoton=0;
+ digitalWrite(led, LOW);
+  er=0;
   
   /*analogWrite(led, 0); // APAGADO
   delay(1000);
