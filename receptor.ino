909b7275b39c4d16623af2d4d42777dc2f27beb5
Ciro-Lopez <cirobenjaminlopez2009@gmail.com>
2026-05-04 16:22:38 -0300

    1er intento COL-FI
    En este commit intentamos dejar atrás el método de un único led LI-FI y pasamos al COL-FI de 3 leds, la razón está detallada en un documento escrito por mi compañero Thiago Grillo.

--- receptor.ino
+++ receptor.ino
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 const int ldrPin = A0;
+const int ldrPin2 = A1;
+const int ldrPin3 = A2;
 const int ledPin = 13;
-int lectura;
+int lectura1, lectura2, lectura3;
 int iguales = 0;
 
 String binario;
@@ -13,53 +15,91 @@ bool espera = false;
 void setup() {
   Serial.begin(9600);
   pinMode(ldrPin, INPUT);
+  pinMode(ldrPin2, INPUT);
+  pinMode(ldrPin3, INPUT);
   Serial.println("--------------");
 }
 
 void loop() {
-  lectura = analogRead(ldrPin);
-  //Serial.println(lectura);
-  if (lectura>15 || iguales==1){
+  lectura1 = analogRead(ldrPin);
+  //Serial.println(lectura1);
+  lectura2 = analogRead(ldrPin2);
+  //Serial.println(lectura2);
+  lectura3 = analogRead(ldrPin3);
+  //Serial.println(lectura3);
+
+
+  if ((lectura1>20 && lectura2>20 && lectura3>20) || iguales==1)
+  {
     if (iguales==0){
       delay(20);
     }
-    iguales=1;
-    if (espera==false && lectura<20){
+    if (espera==false && (lectura1<20 || lectura2<20 || lectura3<20)){
       espera = true;
+      Serial.println("Si");
       delay(49);
+    }
+
+    if (espera==true){
+    if(lectura1>10 && lectura1<30)
+    {
+      binario += '1';
+      Serial.println("1");
+      conteo=conteo+1;
+    }
+    else {if (lectura1>=36){
+      
     }
     else
     {
-      if (espera==true){
-        if (lectura>15){
-          //Serial.println("Se detecto 1");
-          espera = false;
-          binario += '1';
-          conteo=conteo+1;
-        }
-        else{
-          //Serial.println("Se detecto 0");
-          espera = false;
-          binario += '0';
-          conteo=conteo+1;
-
-        }
-      }
+      binario += '0';
+      Serial.println("0");
+      conteo=conteo+1;
     }
     }
-    if (conteo==8){
-      //Serial.println("Se transmitió la letra");}
-
-      /*if (binario="00000000"){
-        exit(0);
-      }*/
+/////////////////////////////////
+    if(lectura2>10 && lectura2<30)
+    {
+      binario += '1';
+      Serial.println("1");
+      conteo=conteo+1;
+    }
+    else {if (lectura2>=36){
       
-      letra+=1;
+    }
+    else
+    {
+      binario += '0';
+      Serial.println("0");
+      conteo=conteo+1;
+    }
+    }
+/////////////////////////////////
+    if(lectura3>10 && lectura3<30)
+    {
+      binario += '1';
+      Serial.println("1");
+      conteo=conteo+1;
+      espera = true;
+    }
+    else {if (lectura3>=36){
+      espera = true;
+    }
+    else
+    {
+      binario += '0';
+      Serial.println("0");
+      conteo=conteo+1;
+      espera = true;
+    }
+    }
+  }
+  /////////////////////////////////
+   if (conteo==8){
       char caracter = (char)strtol(binario.c_str(), NULL, 2);
       Serial.print(caracter);
-      //delay(300);
       conteo=0;
       binario="";
     }
-    
+}
 }
