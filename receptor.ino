c6d5159a744173b379b4611c5d8aee2204a11780
Ciro-Lopez <cirobenjaminlopez2009@gmail.com>
2026-05-06 16:14:39 -0300

    Try receptor 2 leds
    Intento fallido de transmision con 2 leds

--- receptor.ino
+++ receptor.ino
@@ -1,105 +1,91 @@
 #include <stdlib.h>
 const int ldrPin = A0;
 const int ldrPin2 = A1;
-const int ldrPin3 = A2;
-const int ledPin = 13;
-int lectura1, lectura2, lectura3;
+int lectura1, lectura2;
 int iguales = 0;
 
 String binario;
+String vacio = "00000000";
 char total;
 
 int conteo = 0;
 int letra = 0;
-bool espera = false;
+bool espera = true;
 void setup() {
   Serial.begin(9600);
   pinMode(ldrPin, INPUT);
   pinMode(ldrPin2, INPUT);
-  pinMode(ldrPin3, INPUT);
   Serial.println("--------------");
 }
 
 void loop() {
+
   lectura1 = analogRead(ldrPin);
   //Serial.println(lectura1);
   lectura2 = analogRead(ldrPin2);
   //Serial.println(lectura2);
-  lectura3 = analogRead(ldrPin3);
-  //Serial.println(lectura3);
 
-
-  if ((lectura1>20 && lectura2>20 && lectura3>20) || iguales==1)
+  if ((lectura1>7 && lectura2>7) || iguales==1)
   {
+    //Serial.println("Hola");
     if (iguales==0){
-      delay(20);
+      delay(8);
+      iguales=1;
     }
-    if (espera==false && (lectura1<20 || lectura2<20 || lectura3<20)){
+    if (!espera || (lectura1 < 10 && lectura2 < 10))
+    {
       espera = true;
-      Serial.println("Si");
-      delay(49);
+      //Serial.println("Si");
+      //delay(12);
     }
 
-    if (espera==true){
-    if(lectura1>10 && lectura1<30)
+    if (espera==true) 
     {
-      binario += '1';
-      Serial.println("1");
-      conteo=conteo+1;
-    }
-    else {if (lectura1>=36){
-      
-    }
-    else
-    {
-      binario += '0';
-      Serial.println("0");
-      conteo=conteo+1;
-    }
-    }
-/////////////////////////////////
-    if(lectura2>10 && lectura2<30)
-    {
-      binario += '1';
-      Serial.println("1");
-      conteo=conteo+1;
-    }
-    else {if (lectura2>=36){
-      
-    }
-    else
-    {
-      binario += '0';
-      Serial.println("0");
-      conteo=conteo+1;
-    }
-    }
-/////////////////////////////////
-    if(lectura3>10 && lectura3<30)
-    {
-      binario += '1';
-      Serial.println("1");
-      conteo=conteo+1;
-      espera = true;
-    }
-    else {if (lectura3>=36){
-      espera = true;
-    }
-    else
-    {
-      binario += '0';
-      Serial.println("0");
-      conteo=conteo+1;
-      espera = true;
-    }
+
+      if (lectura1>13)
+      {
+        binario += '1';
+        Serial.println("1");
+        conteo=conteo+1;
+        letra+=1;
+      }
+      else
+      {
+        binario += '0';
+        Serial.println("0");
+        conteo=conteo+1;
+      }
+      ///////////////////
+      if (lectura2>13)
+      {
+        binario += '1';
+        Serial.println("1");
+        conteo=conteo+1;
+        espera=true;
+      }
+      else
+      {
+        binario += '0';
+        Serial.println("0");
+        conteo=conteo+1;
+        espera=true;
+      }
+
     }
   }
-  /////////////////////////////////
-   if (conteo==8){
-      char caracter = (char)strtol(binario.c_str(), NULL, 2);
-      Serial.print(caracter);
-      conteo=0;
-      binario="";
+
+  espera=false;
+  delay(12);
+
+  if (conteo==8)
+  {
+    if (binario==vacio)
+    {
+      iguales=0;
     }
-}
+    char caracter = (char)strtol(binario.c_str(), NULL, 2);
+    Serial.print(caracter); 
+    conteo=0;
+    binario="";
+  }
 }
