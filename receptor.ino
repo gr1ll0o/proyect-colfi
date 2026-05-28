af5cc65002871cb02d0a2b0624207d417bc5449d
Ciro-Lopez <cirobenjaminlopez2009@gmail.com>
2026-05-28 17:43:18 -0300

    3rd try Start Bit receptor
    El único problema actual es que el PLL falla cuando en el led2 se repite el mismo bit, el resto parece funcionar

--- receptor.ino
+++ receptor.ino
@@ -10,8 +10,8 @@ char total;
 
 int valDel = 2;
 
-int umbral = 5;
-int numBin1, numAct1;
+int umbral = 2;
+int numBin1, numAct1, delays, tiempoact, diftiempo;
 int numBin2, numAct2;
 bool espera=false;
 
@@ -21,7 +21,7 @@ int numAnt2=0;
 int inicio = 0;
 int conteo = 0;
 
-int tiempo=0;
+int tiempo;
 
 void setup() {
   Serial.begin(115200);
@@ -31,12 +31,10 @@ void setup() {
 }
 
 void loop() {
-  delay(tiempo/2);
   numAct1 = analogRead(ldrPin);
   //Serial.println(numAct1);
   numAct2 = analogRead(ldrPin2);
   //Serial.println(numAct2);
-  delay(tiempo/2);
 
   //Serial.print("NumAct:");
   //Serial.println(numAct1);
@@ -67,6 +65,9 @@ void loop() {
 
   numAnt1=numAct1;
   numAnt2=numAct2;
+  
+  //Serial.println(numBin1);
+
 
   unsigned long t0 = millis();
   while (numBin1==1 && inicio==0)
@@ -79,9 +80,12 @@ void loop() {
     delay(1);
   }
 
-  tiempo = millis() - t0;
 
-  if (tiempo>10){
+  if (espera==false)
+  {
+    tiempo = millis() - t0;
+  }
+  if (tiempo>0 && espera==false){
     Serial.print("Delay detectado:");
     Serial.println(tiempo);
     inicio+=1;
@@ -99,6 +103,7 @@ void loop() {
     conteo+=1;
     Serial.print("Caracter:");
     Serial.println("1");
+
   }
   else
   {
@@ -111,6 +116,24 @@ void loop() {
   
   if (numBin2==1)
   {
+    binario+="0";
+    conteo+=1;
+  unsigned long t0 = millis();
+  while (numBin2==1 && conteo!=8)
+  {
+    numAct2 = analogRead(ldrPin);
+    //Serial.println(numAct2);
+    if (numAct2>numAnt2+umbral){numBin2=1;}
+    if (numAct2<numAnt2-umbral){numBin2=0;}
+    numAnt2=numAct2;
+    delay(1);
+  }
+  tiempoact=millis()-t0;
+  diftiempo=tiempo-tiempoact;
+  if(diftiempo>0)
+  {
+    delay(diftiempo);
+  }
     binario+="1";
     conteo+=1;
     Serial.print("Caracter:");
@@ -119,8 +142,26 @@ void loop() {
   }
   else
   {
-    binario+="0";
-    conteo+=1;
+  binario+="0";
+  conteo+=1;
+  unsigned long t0 = millis();
+  while (numBin2==0 && conteo!=8)
+  {
+
+    numAct2 = analogRead(ldrPin);
+    //Serial.println(numAct2);
+    if (numAct2>numAnt2+umbral){numBin2=1;}
+    if (numAct2<numAnt2-umbral){numBin2=0;}
+    numAnt2=numAct2;
+    delay(1);
+  }
+  tiempoact=millis()-t0;
+  diftiempo=tiempo-tiempoact;
+  if(diftiempo>0)
+  {
+    delay(diftiempo);
+  }
+
     Serial.print("Caracter:");
     Serial.println("0");
   }
@@ -131,10 +172,10 @@ void loop() {
     if (binario==vacio)
     {
       iguales=0;
-      exit(0);
+      asm volatile ("jmp 0");
     }
     char caracter = (char)strtol(binario.c_str(), NULL, 2);
-    Serial.print(caracter); 
+    Serial.println(caracter); 
     conteo=0;
     binario="";
   } 
@@ -142,5 +183,7 @@ void loop() {
   if (espera==true)
   {
     inicio+=1;
+    delay(tiempo);
+    umbral=20;
   }
 }
