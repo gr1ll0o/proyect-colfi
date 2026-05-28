996b3346e62f68c4e9c5f57f168ec07ed256add9
yagosayas123 <yagosayas@gmail.com>
2026-05-28 17:43:10 -0300

    Update emisor.ino funcional 2 leds

--- emisor.ino
+++ emisor.ino
@@ -1,28 +1,37 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-#include <avr/pgmspace.h>
 int led1 = 3;
 int led2 = 6;
 int sincro = 7;
-char testeo[] ="01000001";
+char testeo[] ="10101010";
 bool pasa= false;
 String texto;
 int conteo=0;
-int indice = 0;
 String palabra;
+int espera =1000;
 void setup() 
 {
+  Serial.println("");
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(sincro, OUTPUT);
   Serial.begin(115200);
   Serial.setTimeout(100);
-  Serial.print("INGRESE PALABRA");
+  Serial.println("INGRESE PALABRA");
   while(Serial.available()==0)
   {
   }
   palabra = Serial.readStringUntil('\n');
+  for(int i = 0; i < palabra.length(); i++)
+  {
+    char c = palabra[i];
+    for(int bit = 7; bit >= 0; bit--)
+    {
+      int valor = bitRead(c, bit);
+      texto += char(valor + '0');
+    }
+  }
   digitalWrite(led1, HIGH);
   delay(1000);
   digitalWrite(led1, LOW);
@@ -32,7 +41,8 @@ void setup()
 
 void loop() 
 {
-  while(pasa==false)
+  palabra = Serial.readStringUntil('\n');
+  /*while(pasa==false)
   {
     char c = testeo[conteo];
       if (c == '0')
@@ -58,23 +68,38 @@ void loop()
       {
         pasa=true;
       }
-  }
-  conteo=0;
-  for(int i = 0; i < palabra.length(); i++)
+  }*/
+  char c = texto[conteo];
+  Serial.print(c);
+  if(c=='0')
   {
-    char c = palabra[i];
-    for(int bit = 7; bit >= 0; bit--)
-    {
-      int valor = bitRead(c, bit);
-      texto += char(valor + '0');
-    }
+    digitalWrite(led1, LOW);
   }
-  Serial.print(texto);
-  while(true)
+  else
   {
+    digitalWrite(led1, HIGH);
   }
+
+  conteo+=1;
+  c = texto[conteo];
+  Serial.print(c);
+  if(c=='0')
+  {
+    digitalWrite(led2, LOW);
+  }
+  else
+  {
+    digitalWrite(led2, HIGH);
+  }
+  delay(1000);
   digitalWrite(led1, LOW);
   digitalWrite(led2, LOW);
-  digitalWrite(sincro, LOW);
-  //char c = testeo[conteo];
+  conteo+=1;
+
+  if(conteo >= texto.length() || palabra== "para gil")
+  {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    asm volatile ("jmp 0");
+  }
 }
